09640ce299cfe0c5053b97846d42e02d7b3d4884
Bipul Kumar <vipuldhurvey@gmail.com>
2023-07-22 12:30:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactList from "./components/ContactList";
+
+function App() {
+  return (
+    <div className="App">
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
